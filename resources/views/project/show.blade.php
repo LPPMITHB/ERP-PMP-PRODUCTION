61fb708bacd261d1ec08c0a3312287fb468f91e7
bene <ben.jere0811@gmail.com>
2018-12-18 18:41:09 +0700

    bnrin bug ada di seeder

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -589,7 +589,6 @@
         });
         var project = @json($ganttData);
         var links = @json($links);
-        
        
         gantt.config.columns = [ 
             {name:"text", label:"Task name", width:"*", tree:true,
