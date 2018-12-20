0e9d4ff6dd852fa7be302c40ba36d7f18478348e
Yonathan <mynameyoyoo@gmail.com>
2018-12-20 11:33:41 +0700

    apus seeder project,wbs,activity

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -11,47 +11,16 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-
         \DB::table('pro_project')->delete();
         
         \DB::table('pro_project')->insert(array (
             0 => 
             array (
                 'id' => 1,
-                'number' => 'PROJECT-01',
+                'number' => 'PRO-DUMMY01',
                 'drawing' => NULL,
                 'business_unit_id' => 1,
                 'project_sequence' => 1,
-                'ship_id' => 3,
-                'customer_id' => 2,
-                'name' => 'OIL BARGE 1100KL PROJECT',
-                'description' => 'Real Case',
-                'sales_order_id' => NULL,
-                'planned_start_date' => '2018-12-29',
-                'planned_end_date' => '2019-05-28',
-                'planned_duration' => '151',
-                'actual_start_date' => '2018-12-29',
-                'actual_end_date' => '2019-05-28',
-                'actual_duration' => '151',
-                'progress' => 0.0,
-                'flag' => 'INDONESIA',
-                'class_name' => NULL,
-                'class_contact_person_name' => NULL,
-                'class_contact_person_phone' => NULL,
-                'class_contact_person_email' => NULL,
-                'status' => 1,
-                'user_id' => 5,
-                'branch_id' => 1,
-                'created_at' => '2018-12-19 00:00:00',
-                'updated_at' => '2018-12-19 00:00:00',
-            ),
-            1 => 
-            array (
-                'id' => 2,
-                'number' => 'PRO-DUMMYDUM01',
-                'drawing' => NULL,
-                'business_unit_id' => 1,
-                'project_sequence' => 2,
                 'ship_id' => 5,
                 'customer_id' => 1,
                 'name' => 'Kapal Dummy Boat',
@@ -75,13 +44,13 @@ class ProjectsTableSeeder extends Seeder
                 'created_at' => '2018-12-19 09:25:42',
                 'updated_at' => '2018-12-19 09:25:42',
             ),
-            2 => 
+            1 => 
             array (
-                'id' => 3,
+                'id' => 2,
                 'number' => 'PRO2-DUMMY2',
                 'drawing' => NULL,
                 'business_unit_id' => 2,
-                'project_sequence' => 3,
+                'project_sequence' => 2,
                 'ship_id' => 3,
                 'customer_id' => 2,
                 'name' => 'Repair Dummy',
@@ -106,6 +75,5 @@ class ProjectsTableSeeder extends Seeder
                 'updated_at' => '2018-12-19 10:13:50',
             ),
         ));
-
     }
 }
