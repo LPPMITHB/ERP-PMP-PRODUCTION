356dddf64f0e7c9a60bcf11fdc41ee9883601333
mynameyoyoo <mynameyoyoo@gmail.com>
2019-01-14 11:59:59 +0700

    naikin data

--- app/Http/Controllers/BOMController.php
+++ app/Http/Controllers/BOMController.php
@@ -306,6 +306,7 @@ class BOMController extends Controller
         $datas = json_decode($request->datas);
         $bom_code = self::generateBomCode($datas->project_id);
         $modelBom = Bom::where('wbs_id',$datas->wbs_id)->first();
+
         if(!$modelBom){
             DB::beginTransaction();
             try {
@@ -316,7 +317,6 @@ class BOMController extends Controller
                 $bom->wbs_id = $datas->wbs_id;
                 $bom->branch_id = Auth::user()->branch->id;
                 $bom->user_id = Auth::user()->id;
-
                 if(!$bom->save()){
                     return redirect()->route('bom.create',$bom->id)->with('error', 'Failed Save Bom !');
                 }else{
