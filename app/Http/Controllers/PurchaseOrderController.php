103b93293add597f7e186ad8383a218e604366f5
Benedict Jeremiah <ben.jere0811@gmail.com>
2019-02-04 13:56:03 +0700

    menambah fixing

--- app/Http/Controllers/PurchaseOrderController.php
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -38,9 +38,9 @@ class PurchaseOrderController extends Controller
     {
         $route = $request->route()->getPrefix();
         if($route == "/purchase_order"){
-            $modelPRs = PurchaseRequisition::where('business_unit_id',1)->get();
+            $modelPRs = PurchaseRequisition::where('business_unit_id',1)->pluck('id')->toArray();
         }elseif($route == "/purchase_order_repair"){
-            $modelPRs = PurchaseRequisition::where('business_unit_id',2)->get();
+            $modelPRs = PurchaseRequisition::where('business_unit_id',2)->pluck('id')->toArray();
         }
         $modelPOs = PurchaseOrder::whereIn('purchase_requisition_id',$modelPRs)->get();
 
