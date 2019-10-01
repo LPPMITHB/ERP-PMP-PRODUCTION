044171a290265471529fce21b26db32ad698d568
Jupriano Abelio Ginting <xtabs12@users.noreply.github.com>
2019-10-01 14:07:22 +0700

    ketinggalan

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,54 +24,28 @@ class UsersTableSeeder extends Seeder
             'business_unit_id' => '[1,2,3]',
         ]);
 
-        DB::table('users')->insert([
-            'username' => 'pmp',
-            'name' => 'User PMP',
-            'email' => 'pmp@pmp.com',
-            'type' => 1,
-            'role_id' => 4,
-            'branch_id' => 1,
-            'password' => bcrypt('patria'),
-            'business_unit_id' => '[1]',
-        ]);
+        // DB::table('users')->insert([
+        //     'username' => 'pmp',
+        //     'name' => 'User PMP',
+        //     'email' => 'pmp@pmp.com',
+        //     'type' => 1,
+        //     'role_id' => 4,
+        //     'branch_id' => 1,
+        //     'password' => bcrypt('patria'),
+        //     'business_unit_id' => '[1]',
+        // ]);
 
-        DB::table('users')->insert([
-            'username' => 'pami',
-            'name' => 'User PAMI',
-            'email' => 'pami@pami.com',
-            'type' => 1,
-            'role_id' => 5,
-            'branch_id' => 2,
-            'password' => bcrypt('patria'),
-            'business_unit_id' => '[2]',
-        ]);
+        // DB::table('users')->insert([
+        //     'username' => 'pami',
+        //     'name' => 'User PAMI',
+        //     'email' => 'pami@pami.com',
+        //     'type' => 1,
+        //     'role_id' => 5,
+        //     'branch_id' => 2,
+        //     'password' => bcrypt('patria'),
+        //     'business_unit_id' => '[2]',
+        // ]);
 
-        DB::table('users')->insert([
-            'username' => 'johndoe',
-            'name' => 'John Doe',
-            'email' => 'johndoe@mail.com',
-            'type' => 2,
-            'role_id' => 2,
-            'branch_id' => 2,
-            'password' => bcrypt('johndoe'),
-            'business_unit_id' => '[1,2,3]',
-        ]);
-
-        $modelCustomer = Customer::all();
-        foreach($modelCustomer as $customer){
-            DB::table('users')->insert([
-                'username' => $customer->code,
-                'name' => $customer->name,
-                'email' => $customer->email,
-                'type' => 3,
-                'role_id' => 3,
-                'branch_id' => $customer->branch_id,
-                'password' => bcrypt('patria'),
-                'business_unit_id' => '[2]',
-            ]);
-            $user = User::where('username',$customer->code)->first();
-            $customer->user_id = $user->id;
-            $customer->update();
-        }
+        
     }
 }
