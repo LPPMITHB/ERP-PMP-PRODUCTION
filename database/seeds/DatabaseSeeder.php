1ad1e8a861c621acb243085fc99cc429f440270c
Jupriano Abelio Ginting <xtabs12@users.noreply.github.com>
2019-10-01 14:11:19 +0700

    lagi

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(ConfigurationsTableSeeder::class);
         $this->call(SidenavsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
     }
 }
