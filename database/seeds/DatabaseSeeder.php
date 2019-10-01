a6e7e50e6e096848665232a6a453c5bbb752006c
Jupriano Abelio Ginting <xtabs12@users.noreply.github.com>
2019-10-01 14:13:15 +0700

    naikin roles seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(MenusTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ConfigurationsTableSeeder::class);
         $this->call(SidenavsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
+        
     }
 }
